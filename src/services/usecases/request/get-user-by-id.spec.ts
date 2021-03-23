a48e64527f7b668f4f97f4c04fcca62507bb4e22
eduviictor <eduvictornobrega@gmail.com>
2021-03-23 18:32:02 -0300

    test: add tests for GetUserById

---
+++ src/services/usecases/request/get-user-by-id.spec.ts
@@ -0,0 +1,66 @@
+import { RequestUserById } from '@/domain/usecases/request-user-by-id';
+import { UserRequest } from '../../protocols/request/user-request';
+import { GetUserById } from './get-user-by-id';
+
+const makeUserRequest = (): RequestUserById => {
+  class UserRequestStub implements RequestUserById {
+    async getById(): Promise<any> {
+      const fakeUser = {
+        id: 1,
+        name: 'valid_name',
+      };
+      return await new Promise((resolve) => resolve(fakeUser));
+    }
+  }
+
+  return new UserRequestStub();
+};
+
+interface SutTypes {
+  sut: GetUserById;
+  userRequestStub: UserRequest;
+}
+
+const makeSut = (): SutTypes => {
+  const userRequestStub = makeUserRequest();
+  const sut = new GetUserById(userRequestStub);
+  return {
+    sut,
+    userRequestStub,
+  };
+};
+
+describe('GetPlanetsById Usecase', () => {
+  test('Should call GetUserById', async () => {
+    const { sut, userRequestStub } = makeSut();
+    const getByIdSpy = jest.spyOn(userRequestStub, 'getById');
+
+    await sut.getById(1);
+
+    expect(getByIdSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test('Should GetUserById return an user with success', async () => {
+    const { sut } = makeSut();
+
+    const planets = await sut.getById(1);
+
+    expect(planets).toEqual({
+      id: 1,
+      name: 'valid_name',
+    });
+  });
+
+  test('Should throw if UserRequest throws', async () => {
+    const { sut, userRequestStub } = makeSut();
+    jest
+      .spyOn(userRequestStub, 'getById')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const promise = sut.getById(1);
+
+    await expect(promise).rejects.toThrow();
+  });
+});
