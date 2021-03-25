532782f78b5344f894e1c148fe8b1d779b7d3f82
eduviictor <eduvictornobrega@gmail.com>
2021-03-25 13:17:03 -0300

    test: add tests for GetDebtByIdController

---
+++ src/main/routes/debts-routes.test.ts
@@ -0,0 +1,57 @@
+import { MongoHelper } from '@/infra/db/helpers/mongo-helper';
+import request from 'supertest';
+import app from '../config/app';
+
+const date = new Date().toLocaleDateString('en-CA', {
+  day: '2-digit',
+  year: 'numeric',
+  month: '2-digit',
+});
+
+describe('Debt Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const debtCollection = await MongoHelper.getCollection('debts');
+    await debtCollection.deleteMany({});
+  });
+
+  test('Should create an debt on success', async () => {
+    await request(app)
+      .post('/debts')
+      .send({
+        user_id: 1,
+        reason: 'any_reason',
+        date,
+        amount: '15.99',
+      })
+      .expect(200);
+  });
+
+  test('Should return a planet based on its id', async () => {
+    const debtCollection = await MongoHelper.getCollection('debts');
+    const debtInDb = await debtCollection.insertOne({
+      user_id: 1,
+      reason: 'any_reason',
+      date: String(date),
+      amount: '15.99',
+    });
+
+    const id = debtInDb.ops[0]._id;
+    await request(app)
+      .get(`/debts/${id}`)
+      .expect({
+        user_id: 1,
+        reason: 'any_reason',
+        date: String(date),
+        amount: '15.99',
+        id: String(id),
+      });
+  });
+});
