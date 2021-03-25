bf304052872f586152ae3162c04680efe2b094f4
eduviictor <eduvictornobrega@gmail.com>
2021-03-25 12:43:45 -0300

    feat: add getById in DebtMongoRepository

--- src/infra/db/debt-repository/debt.ts
+++ src/infra/db/debt-repository/debt.ts
@@ -1,12 +1,26 @@
 import { DebtModel } from '@/domain/models/debt';
 import { AddDebtModel } from '@/domain/usecases/add-debt';
 import { AddDebtRepository } from '@/services/protocols/db/db-add-debt-repository';
+import { GetDebtByIdRepository } from '@/services/protocols/db/db-get-debt-by-id-repository';
+import { ObjectId } from 'bson';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class DebtMongoRepository implements AddDebtRepository {
+export class DebtMongoRepository
+  implements AddDebtRepository, GetDebtByIdRepository {
   async add(debtData: AddDebtModel): Promise<DebtModel> {
     const debtCollection = await MongoHelper.getCollection('debts');
     const result = await debtCollection.insertOne(debtData);
     return MongoHelper.map(result.ops[0]);
+  }
+
+  async getById(id: string): Promise<DebtModel> {
+    const planetCollection = await MongoHelper.getCollection('debts');
+    const result = await planetCollection.findOne({
+      _id: new ObjectId(id),
+    });
+    if (!result) {
+      return null;
+    }
+    return MongoHelper.map(result);
   }
 }
